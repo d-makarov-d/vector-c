ba83647605c6d269bd87c624db07143d22e7b0ae
Danila Makarov <danila.makarov@gmail.com>
2021-12-06 23:11:10 +0300

    - Restructured file tree as Python package

---
+++ README.md
@@ -0,0 +1,4 @@
+# 3D Euclidean vector
+Simple Euclidean 3-dimensional vector class, implemented using Python C extension.
+### Installation
+`python setup.py install`
--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, Extension
 
-module = Extension('vector', sources=['src/vector.c', 'src/utils.c'])
+module = Extension('vector', sources=['src/vector/src/vector.c', 'src/vector/src/utils.c'])
 
 setup(
-    name='Vector',
-    version='1.0',
-    description='Cpp implementation for vector class',
+    name='vector-c',
+    version='1.0.0',
+    description='Cpp implementation for 3-dimensional vector',
     ext_modules=[module]
 )
