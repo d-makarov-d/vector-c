7a0c572ec65c30e98f52525e21c6dc1f89258c14
Danila Makarov <danila.makarov@gmail.com>
2021-12-07 15:16:52 +0300

    - version update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ module = Extension('vector', sources=['src/vector/src/vector.c', 'src/vector/src
 
 setup(
     name='vector-c',
-    version='1.0.0',
+    version='1.1.0',
     description='Cpp implementation for 3-dimensional vector',
     ext_modules=[module]
 )
