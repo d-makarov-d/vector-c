5513d64e2fb540a0977922d9ba9c0e67145ceb77
Danila Makarov <danila.makarov@gmail.com>
2021-12-22 21:36:03 +0300

    _ Switched to ptyphon object hash implementation to avoid internal functions usage

--- src/vector/src/utils.c
+++ src/vector/src/utils.c
@@ -6,43 +6,9 @@ bool isnan(double val) {
     return val != val;
 }
 
-/* Hash for double arrays.
-From https://github.com/python/cpython/blob/main/Objects/tupleobject.c
-CPython tuple hash implementation
-*/
-#if SIZEOF_PY_UHASH_T > 4
-#define _PyHASH_XXPRIME_1 ((Py_uhash_t)11400714785074694791ULL)
-#define _PyHASH_XXPRIME_2 ((Py_uhash_t)14029467366897019727ULL)
-#define _PyHASH_XXPRIME_5 ((Py_uhash_t)2870177450012600261ULL)
-#define _PyHASH_XXROTATE(x) ((x << 31) | (x >> 33))  /* Rotate left 31 bits */
-#else
-#define _PyHASH_XXPRIME_1 ((Py_uhash_t)2654435761UL)
-#define _PyHASH_XXPRIME_2 ((Py_uhash_t)2246822519UL)
-#define _PyHASH_XXPRIME_5 ((Py_uhash_t)374761393UL)
-#define _PyHASH_XXROTATE(x) ((x << 13) | (x >> 19))  /* Rotate left 13 bits */
-#endif
-
-/* Tests have shown that it's not worth to cache the hash value, see
-   https://bugs.python.org/issue9685 */
 Py_hash_t arr_hash(double v[], Py_ssize_t len) {
-    Py_uhash_t acc = _PyHASH_XXPRIME_5;
-    for (Py_ssize_t i = 0; i < len; i++) {
-        Py_uhash_t lane = _Py_HashDouble(v[i]);
-        if (lane == (Py_uhash_t)-1) {
-            return -1;
-        }
-        acc += lane * _PyHASH_XXPRIME_2;
-        acc = _PyHASH_XXROTATE(acc);
-        acc *= _PyHASH_XXPRIME_1;
-    }
-
-    /* Add input length, mangled to keep the historical value of hash(()). */
-    acc += len ^ (_PyHASH_XXPRIME_5 ^ 3527539UL);
-
-    if (acc == (Py_uhash_t)-1) {
-        return 1546275796;
-    }
-    return acc;
+    PyObject *tup = Py_BuildValue("(ddd)", v[0], v[1], v[2]);
+    return PyObject_Hash(tup);
 }
 
 bool arr_cmp(double a[], double b[], Py_ssize_t n) {
